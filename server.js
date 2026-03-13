343778c1a45e8a7f5100a3cdabe3dce625f493f3
ChidiebereMichael18 <mikeyxofficil1234@gmail.com>
2026-03-13 21:18:18 +0100

    whatt

--- server.js
+++ server.js
@@ -17,16 +17,7 @@ const chatRoutes = require('./routes/chatRoutes');
 const app = express();
 
 // Middleware
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-    credentials: true,
-  },
-  {
-    origin: 'https://ea-coach-v.vercel.app',
-    credentials: true,
-  }
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
