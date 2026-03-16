892545721fc26d2174c13bbd7cff2e83c77c81f0
ChidiebereMichael18 <mikeyxofficil1234@gmail.com>
2026-03-16 23:50:01 +0100

    backend updates

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,12 +11,12 @@ const protect = async (req, res, next) => {
       req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
-      res.status(401).json({ message: 'Not authorized, token failed' });
+      res.status(401).json({ message: 'Not authorized, Please Sign in' });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
+    res.status(401).json({ message: 'Not authorized, Please Sign in' });
   }
 };
 
